10fc39a74d4f3a1231bc3c4a8a116b10d4f80ea1
sayandebnath-creator <simon.klg123@gmail.com>
2026-09-16 01:27:07 +0530

    chore(database): enable detailed GORM SQL logging

--- internal/database/database.go
+++ internal/database/database.go
@@ -8,6 +8,7 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -23,7 +24,9 @@ func Connect(cfg *config.Config) {
 		cfg.DBSSLMode,
 	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		log.Fatal("❌ Failed to connect to database:", err)
 	}
